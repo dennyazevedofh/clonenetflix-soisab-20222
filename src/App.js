626cb6bc95904e22a92b179b8b3a0bdc50fb9137
DennyAzevedo <denny_af@yahoo.com.br>
2022-08-27 11:57:00 -0300

    Finalizado

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tmdb from './Tmdb'
 
 import FeaturedMovie  from './components/FeaturedMovie'
 import Header from './components/Header'
+import MovieRow from './components/MovieRow'
 
 export default () => {
   const [blackHeader, setBlackHeader] = useState(false)
@@ -14,12 +15,10 @@ export default () => {
     const loadAll = async () => {
       let list = await Tmdb.getHomeList()
       setMovieList(list)
-
       let originals = list.filter(i => i.slug === 'originals')
       let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
       let chosen = originals[0].items.results[randomChosen]
       let choseInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
-      console.log(choseInfo)
       setFeaturedData(choseInfo)
     }
     loadAll()
@@ -51,13 +50,23 @@ export default () => {
       }
 
       <div className="Lists">
-
+        {movieList.map((item, key) =>
+          <MovieRow key={key} title={item.title} items ={item.items} />
+        )}
       </div>
+
       <footer>
         Feito por Denny Azevedo <br />
         Direitos de imagem para Netflix <br />
         Dados pegos do site Themoviedb.org
       </footer>
+
+      {movieList.length <= 0 &&
+        <div className="Loadimg">
+          <img src="https://media.wired.com/photos/592744d3f3e2356fd800bf00/master/w_2560%2Cc_limit/Netflix_LoadTime.gif" alt="Carregando" />
+        </div>
+      }
+      
     </div>
   )
 }
