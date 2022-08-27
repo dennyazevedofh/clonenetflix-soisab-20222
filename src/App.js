24f5b40e577357fb40edf52316cac4624609a6ff
DennyAzevedo <denny_af@yahoo.com.br>
2022-08-27 09:59:54 -0300

    Obtencao de dados e criacao do banner

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import Tmdb from './Tmdb'
 
-function App() {
+import FeaturedMovie  from './components/FeaturedMovie'
+import Header from './components/Header'
+
+export default () => {
+  const [blackHeader, setBlackHeader] = useState(false)
+  const [movieList, setMovieList] = useState([])
+  const [featuredData, setFeaturedData] = useState(null)
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList()
+      setMovieList(list)
+
+      let originals = list.filter(i => i.slug === 'originals')
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+      let chosen = originals[0].items.results[randomChosen]
+      let choseInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
+      console.log(choseInfo)
+      setFeaturedData(choseInfo)
+    }
+    loadAll()
+  }, [])
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true)
+      } else {
+        setBlackHeader(false)
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener)
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener)
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="page">
 
-export default App;
+      <Header black={blackHeader} />
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
+
+      <div className="Lists">
+
+      </div>
+      <footer>
+        Feito por Denny Azevedo <br />
+        Direitos de imagem para Netflix <br />
+        Dados pegos do site Themoviedb.org
+      </footer>
+    </div>
+  )
+}
